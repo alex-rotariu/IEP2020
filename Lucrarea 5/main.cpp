b85c84fc460c5eb29ce477377a147e7da8c0b5a8
Alex <rotariualex10@gmail.com>
2020-11-20 20:11:23 +0200

    Added classes

--- Lucrarea 5/main.cpp
+++ Lucrarea 5/main.cpp
@@ -1,23 +1,46 @@
 #include <iostream>
+
 using namespace std;
 
 class Pizza{
     private:
-        //string name;
+        string name;
         int price;
-        //string topping;
     
-    //public:
-        //Pizza() : name(), price(15), topping(){
-            //cout<<name << ":"<< price << " " << topping;
-            //};
-        //Pizza(string name, int price, string topping);
-        //Pizza(const Pizza& rhs);
-        //Pizza& operator=(const Pizza& rhs);
-        
+    public:
+        Pizza() : name("pizzoasa"), price(15){};
+        Pizza(string n, int p):
+            name(n),
+            price(p),
+        {};
+        Pizza(const Pizza& rhs);
+        Pizza& operator=(const Pizza& rhs){
+            this->name=rhs.name;
+            this->price=rhs.price;
+            return *this;
+        };
+        void toString(){
+            cout<<name<<" "<<price;
+        }  
 };
 
+class Capriciosa : public Pizza{
+
+}
+
+class Diavola : public Pizza {
+
+}
+
+class Sos {
+
+}
+
 int main(){
-    Pizza p1;
+    Pizza *p1 = new Pizza("bomba",10);
+    Pizza p2;
+    p1->toString();
+    cout<<endl;
+    p2.toString();
     return 0;
 }
