428767be30086359f672253ca589639ffbf4d770
BiancaRadulescu <bradulescu10@gmail.com>
2020-11-20 21:40:43 +0200

    Proiect2

---
+++ Lucrarea 5/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Pizza{
+    private:
+        //string name;
+        int price;
+        //string topping;
+    
+    //public:
+        //Pizza() : name(), price(15), topping(){
+            //cout<<name << ":"<< price << " " << topping;
+            //};
+        //Pizza(string name, int price, string topping);
+        //Pizza(const Pizza& rhs);
+        //Pizza& operator=(const Pizza& rhs);
+        
+};
+
+int main(){
+    Pizza p1;
+    return 0;
+}
