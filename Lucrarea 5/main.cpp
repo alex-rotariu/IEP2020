80d7855140b4f79820cc5f0b9be90745c86ab7fe
BiancaRadulescu <bradulescu10@gmail.com>
2020-11-21 19:53:42 +0200

    added implementation for some classes

--- Lucrarea 5/main.cpp
+++ Lucrarea 5/main.cpp
@@ -1,46 +1,68 @@
 #include <iostream>
 
 using namespace std;
+class Sos {
+    private:
+        string tipSos;
+    
+    public:
+        Sos(string ts): tipSos(ts){};
+        string getTipSos(){
+            return this->tipSos;
+        }
+};
 
 class Pizza{
     private:
         string name;
         int price;
+        Sos *sos;
     
     public:
         Pizza() : name("pizzoasa"), price(15){};
-        Pizza(string n, int p):
+
+        Pizza(string n, int p, Sos *s):
             name(n),
             price(p),
+            sos(s)
         {};
+
         Pizza(const Pizza& rhs);
+
         Pizza& operator=(const Pizza& rhs){
             this->name=rhs.name;
             this->price=rhs.price;
+
+            Sos *orig = sos;
+            this->sos=new Sos(*rhs.sos);
+            delete orig;
+
             return *this;
         };
+
+        void toStringDefault(){
+            cout<<"Pizza:"<<name<<"-> "<<price<< "lei";
+        }
         void toString(){
-            cout<<name<<" "<<price;
-        }  
+            cout<<"Pizza:"<<name<<"-> "<<price<< "lei cu "<<sos->getTipSos();
+        }    
 };
 
 class Capriciosa : public Pizza{
 
-}
+};
 
 class Diavola : public Pizza {
 
-}
-
-class Sos {
-
-}
+};
 
 int main(){
-    Pizza *p1 = new Pizza("bomba",10);
+    Sos *s1 = new Sos("sos de muiere(comanda speciala pt Roata)");
+
+    Pizza *p1 = new Pizza("bomba",10,s1);
     Pizza p2;
     p1->toString();
     cout<<endl;
-    p2.toString();
+    p2.toStringDefault();
     return 0;
 }
